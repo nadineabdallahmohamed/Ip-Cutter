555046d999da119b6602750210d739899ba18290
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-08 18:43:49 +0200

    Create IP_Cutter.java

---
+++ IP_Cutter.java
@@ -0,0 +1,17 @@
+package ip_cutter;
+
+import java.util.*;
+
+public class IPCUTTER {
+    public static void main(String[] args)
+    {
+        Scanner myObj = new Scanner(System.in);
+        String name = myObj.nextLine();
+        String[] result = name.split("[.]");
+        int i;
+        for(i=0;i<4;++i){
+            System.out.println(result[i]); 
+        }
+          
+    }
+}
