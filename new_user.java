784648e6e33ee5720da53609cb12fb98789845d3
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-11 23:07:07 +0200

    Create new_user.java

---
+++ new_user.java
@@ -0,0 +1,28 @@
+package cutter;
+
+public class Merchant extends Customer {
+	public Merchant(String name) {
+		
+		super(name);
+	}
+	private  float totlepurachse;
+
+	public float getTotlepurachse() {
+		return totlepurachse;
+	}
+
+	public void setTotlepurachse(float totlepurachse) {
+		this.totlepurachse = totlepurachse;
+	}
+	
+	
+	public float calculate() {
+		
+		return totlepurachse*250;
+	}
+	
+	
+	
+	
+
+}
